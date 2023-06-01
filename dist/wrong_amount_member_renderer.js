2b5e4cb5513eb52d875006a142213ac32edbeee4
chikashi <chikashi@dear-reality.com>
2023-06-01 22:28:16 +0200

    transaction all

--- dist/wrong_amount_member_renderer.js
+++ dist/wrong_amount_member_renderer.js
@@ -8,26 +8,38 @@ export class WrongAmountMemberRenderer {
     render(matchedRecords) {
         this.tbody.innerHTML = "";
         matchedRecords.forEach((matchedRecord) => {
-            var _a;
-            let tableRow = document.createElement("tr");
             const member = matchedRecord.member;
-            const transaction = matchedRecord.transactions[0];
-            const requiredAmount = (_a = this.fixedAmount) !== null && _a !== void 0 ? _a : member.childrenState.getQuarterlyTuition();
-            const difference = transaction.amount - requiredAmount;
-            const difText = difference < 0 ? Math.abs(difference).toString() + "不足" : difference.toString() + "余剰";
-            const tableData = createTableData([
-                member.nameInJapanese.lastName,
-                member.nameInJapanese.firstName,
-                member.partner.name.lastName,
-                requiredAmount.toString(),
-                transaction.amount.toString(),
-                difText,
-                createSimpleDateString(transaction.date),
-                transaction.payer,
-                transaction.purpose
-            ]);
-            tableData.forEach(data => tableRow.append(data));
-            this.tbody.append(tableRow);
+            const transactions = matchedRecord.transactions;
+            transactions.forEach((transaction, index) => {
+                var _a;
+                let tableRow = document.createElement("tr");
+                const requiredAmount = (_a = this.fixedAmount) !== null && _a !== void 0 ? _a : member.childrenState.getQuarterlyTuition();
+                const difference = transaction.amount - requiredAmount;
+                const difText = difference < 0 ? Math.abs(difference).toString() + "不足" : difference.toString() + "余剰";
+                const tableData = createTableData([
+                    member.nameInJapanese.lastName,
+                    member.nameInJapanese.firstName,
+                    member.partner.name.lastName,
+                    requiredAmount.toString(),
+                    transaction.amount.toString(),
+                    difText,
+                    createSimpleDateString(transaction.date),
+                    transaction.payer,
+                    transaction.purpose
+                ]);
+                if (transactions.length > 1) {
+                    if (index === 0) {
+                        for (let i = 0; i < 4; ++i)
+                            tableData[i].rowSpan = transactions.length;
+                    }
+                    else {
+                        for (let i = 0; i < 4; ++i)
+                            tableData.shift();
+                    }
+                }
+                tableData.forEach(data => tableRow.append(data));
+                this.tbody.append(tableRow);
+            });
         });
         this.span.textContent = matchedRecords.length.toString();
     }
--- src/wrong_amount_member_renderer.ts
+++ src/wrong_amount_member_renderer.ts
@@ -16,28 +16,38 @@ export class WrongAmountMemberRenderer{
         this.tbody.innerHTML = "";
 
         matchedRecords.forEach((matchedRecord)=>{
-            let tableRow = document.createElement("tr");
             const member = matchedRecord.member;
-            const transaction = matchedRecord.transactions[0];
+            const transactions = matchedRecord.transactions;
 
-            const requiredAmount = this.fixedAmount ?? member.childrenState.getQuarterlyTuition();
-            const difference = transaction.amount - requiredAmount;
-            const difText = difference < 0 ? Math.abs(difference).toString() + "不足" : difference.toString() + "余剰"
+            transactions.forEach((transaction, index)=> {
+                let tableRow = document.createElement("tr");
+                const requiredAmount = this.fixedAmount ?? member.childrenState.getQuarterlyTuition();
+                const difference = transaction.amount - requiredAmount;
+                const difText = difference < 0 ? Math.abs(difference).toString() + "不足" : difference.toString() + "余剰"
 
-            const tableData = createTableData([
-                member.nameInJapanese.lastName,
-                member.nameInJapanese.firstName,
-                member.partner.name.lastName,
-                requiredAmount.toString(),
-                transaction.amount.toString(),
-                difText,
-                createSimpleDateString(transaction.date),
-                transaction.payer,
-                transaction.purpose
+                const tableData = createTableData([
+                    member.nameInJapanese.lastName,
+                    member.nameInJapanese.firstName,
+                    member.partner.name.lastName,
+                    requiredAmount.toString(),
+                    transaction.amount.toString(),
+                    difText,
+                    createSimpleDateString(transaction.date),
+                    transaction.payer,
+                    transaction.purpose
                 ]);
-
-            tableData.forEach(data => tableRow.append(data));
-            this.tbody.append(tableRow);
+                if(transactions.length > 1){
+                    if(index === 0) {
+                        for (let i = 0; i < 4; ++i)
+                            (tableData[i] as HTMLTableDataCellElement).rowSpan = transactions.length;
+                    }else{
+                        for (let i = 0; i < 4; ++i)
+                            tableData.shift();
+                    }
+                }
+                tableData.forEach(data => tableRow.append(data));
+                this.tbody.append(tableRow);
+            });
         });
 
         this.span.textContent = matchedRecords.length.toString();
